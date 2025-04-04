ea3b5a54f82724c0a6133c1376a31c4b6166fe16
MacBook Air <macbookair@MacBooks-Laptop.local>
2025-04-04 14:38:35 +0700

    update

--- resources/views/livewire/sales/applications/application-list.blade.php
+++ resources/views/livewire/sales/applications/application-list.blade.php
@@ -60,9 +60,12 @@
                         <div class="card bg-primary rounded d-flex">
                             <div class="d-flex mb-3 justify-content-between">
                                 <div class="p-2 d-flex">
-                                    <div class="mt-1 text-white">{{__('Noted For')}} {{$app->created_at->format('Y-m-d')}}</div>
+                                    <div class="mt-1 text-white">{{__('Expense')}}
+                                        {{$app->created_at->format('Y-m-d')}}
+                                    </div>
                                 </div>
-                                <div class="d-flex justify-content-end pointer" wire:click="btn_edit_application({{$app->id}})">
+                                <div class="d-flex justify-content-end pointer"
+                                    wire:click="btn_edit_application({{$app->id}})">
                                     <div class="p-2">
                                         <div class="col-lg-12 col-sm-6 text-info">
                                             <i class="bi bi-pencil-square"></i>
@@ -77,15 +80,16 @@
                                 <h6 class="p-1"> <i class="bi bi-brightness-alt-high-fill p-2"></i> Breakfast</h6>
                                 <h6 class="p-1"> <i class="bi bi-brightness-high-fill p-2"></i> Lunch</h6>
                                 <h6 class="p-1"> <i class="bi bi-brightness-alt-low-fill p-2"></i> Dinner</h6>
-                                <h6 class="p-1"> <i class="bi bi-brightness-high-fill p-2"></i> Coffee X <strong class="text-danger">({{$app->amount_coffee}})</strong></h6>
+                                <h6 class="p-1"> <i class="bi bi-brightness-high-fill p-2"></i> Coffee X <strong
+                                        class="text-danger">({{$app->amount_coffee}})</strong></h6>
                                 <h6 class="p-1"> <i class="bi bi-brightness-alt-low-fill p-2"></i> Gasoline</h6>
                             </div>
                             <div class="col-lg-7 p-2" style="margin-top: -17px;">
                                 <h6>Item Cost</h6>
-                                <h6 class="mt-4"> <strong>៛ {{number_format($app->breakfast,2)}}</strong></h6>
+                                <h6 class="mt-3"> <strong>៛ {{number_format($app->breakfast,2)}}</strong></h6>
                                 <h6 class="mt-3"> <strong>៛ {{number_format($app->lunch,2)}}</strong></h6>
-                                <h6 class="mt-3"> <strong>៛ {{number_format($app->dinner,2)}}</strong></h6>
-                                <h6 class="mt-3"> <strong>៛ {{number_format($app->coffee_price,2)}}</strong></h6>
+                                <h6 class="mt-4"> <strong>៛ {{number_format($app->dinner,2)}}</strong></h6>
+                                <h6 class="mt-3"> <strong>$ {{number_format($app->coffee_price,2)}}</strong></h6>
                                 <h6 class="mt-3"> <strong>៛ {{number_format($app->gasoline_price,2)}}</strong></h6>
                             </div>
                             <div class="col-lg-12">
@@ -110,28 +114,28 @@
     @endif
 </div>
 <script type="text/javascript">
-    //--selected date--
-    jQuery(document).ready(function() {
-        jQuery('#start_date').datepicker({
-            autoclose: true,
-            todayHighlight: true,
-            language: 'XX',
-            format: "yyyy-mm-dd",
-            minDate: '-12m',
-            maxDate: '0',
-        });
-        jQuery('#end_date').datepicker({
-            autoclose: true,
-            todayHighlight: true,
-            language: 'XX',
-            uiLibrary: 'bootstrap5',
-            format: "yyyy-mm-dd",
-            minDate: '-12m',
-            maxDate: '0',
-        });
-        jQuery('#start_date, #end_date').on('change', function() {
-            @this.set('start_date', jQuery('#start_date').val());
-            @this.set('end_date', jQuery('#end_date').val());
-        });
+//--selected date--
+jQuery(document).ready(function() {
+    jQuery('#start_date').datepicker({
+        autoclose: true,
+        todayHighlight: true,
+        language: 'XX',
+        format: "yyyy-mm-dd",
+        minDate: '-12m',
+        maxDate: '0',
     });
+    jQuery('#end_date').datepicker({
+        autoclose: true,
+        todayHighlight: true,
+        language: 'XX',
+        uiLibrary: 'bootstrap5',
+        format: "yyyy-mm-dd",
+        minDate: '-12m',
+        maxDate: '0',
+    });
+    jQuery('#start_date, #end_date').on('change', function() {
+        @this.set('start_date', jQuery('#start_date').val());
+        @this.set('end_date', jQuery('#end_date').val());
+    });
+});
 </script>
